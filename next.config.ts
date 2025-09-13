ece01849b8bb4e668a4966ad38777aa90c6cceaa
hoongyan <hoongyan97@gmail.com>
2025-09-14 01:45:35 +0800

    ignore eslint during builds for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
-
+    eslint: {
+        ignoreDuringBuilds: true, // Skip ESLint during production builds
+      },
 };
 
 export default nextConfig;
